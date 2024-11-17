1d160cacf8e917f60f78df980c9a9e854d298a04
Brxnni <arne.norvin@gmail.com>
2024-11-17 13:38:04 +0100

    Line detection works!!! (half of it)

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # The Plan
+
 ## Lines
-- Find right contrast value such that enough lines are found for the following steps to make sense
-- Assign a thickness and angle every line
-- Split lines into "thick" and "thin" ones
-- Try to find thick lines that are parallel (i.e. have a similar angle)
+
+1. Find right contrast value such that enough lines are found for the following steps to make sense
+    - Things that dont work: Harris Corner Detection
+2. Assign a thickness and angle every line
+3. Split lines into "thick" and "thin" ones
+4. Try to find thick lines that are parallel (i.e. have a similar angle)
 
 ## Circle
-- Find pixes of the right color (+- tolerance of course), starting from the center of the image
+
+1. Find pixes of the right color (+- tolerance of course), starting from the center of the image
 
 ## Final
-- Find the lines closest to the purple circle -> two thick lines are 1cm apart
-- Use those to calculate the real-world area of the circle
+
+1. Find the lines closest to the purple circle -> two thick lines are 1cm apart
+2. Use those to calculate the real-world area of the circle
--- main.py
+++ main.py
@@ -1,23 +1,102 @@
-import cv2
+import colorsys
+import random
 import numpy as np
+import math
+import cv2
+import os
 
 import pathlib
 LOCAL = pathlib.Path(__file__).parent
-image = cv2.imread(LOCAL / "w12images" / "WhatsApp Image 2024-11-15 at 17.38.09 (1).jpeg")
+images = [ cv2.imread(LOCAL / "w12images" / path) for path in os.listdir(LOCAL / "w12images") ]
 
-images = [ cv2.imread(LOCAL / "w12images" / a) for a in [
-	"WhatsApp Image 2024-11-15 at 17.38.35 (4).jpeg",
-	"WhatsApp Image 2024-11-15 at 17.38.09.jpeg",
-	"WhatsApp Image 2024-11-15 at 17.38.49 (2).jpeg"
-] ]
+def randomColor():
+	return tuple(
+		# RGB -> BGR
+		reversed(
+			# RGB(normalized) -> RGB(0-255,0-255,0-255)
+			[ round(i*255) for i in colorsys.hsv_to_rgb(
+				# HSV(normalized) -> RGB(normalized)
+				random.random(), 1, 1)
+			]
+		)
+	)
 
-# cv2.imshow("Original image", image)
+def isSquare(contour):
+	area = cv2.contourArea(contour)
+	x, y, width, height = cv2.boundingRect(contour)
+	aspect_ratio = float(width) / height
 
-# Crop to remove table
-# image = image[37:2387, 56:7079]
+	hull = cv2.convexHull(contour)
+	hull_area = cv2.contourArea(hull)
+	if hull_area == 0: return False
+	solidity = float(area) / hull_area
+
+	print(solidity)
+	# return solidity > 0.95
+	return abs(aspect_ratio - 1) < 0.1
+
+def findSquareContours(i):
+	img = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+
+	blurred = cv2.GaussianBlur(img, (5, 5), 0)
+	thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, THRESH_VALUES.get(i, 9), 2)
+	thresh[thresh > 0] = 255
+
+	# Find lines in thresh
+	contours, _ = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+	contours = [ c for c in contours if cv2.contourArea(c) > 10 ]
+	contours = [ c for c in contours if isSquare(c) ]
+	a = sorted([ cv2.contourArea(c) for c in contours ])
+	print(i, "|", len(contours))
+	
+	thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+	for idx, contour in enumerate(contours):
+		cv2.drawContours(image=thresh, contours=contours, contourIdx=idx, color=randomColor(), thickness=1)
+
+	cv2.imshow(f"Detected Edges{i}", thresh)
+
+def findGridLines(i):
+	img = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+
+	# vv THIS CAN WORK!! FIND THRESH VALUES SUCH THAT ALL TINY 1mm² SQUARES ARE SEPERATE,
+	# THEN FIND ALL WHITE BLOBS (WHICH SHOULD INCLUDE SQUARES), FILTER FOR THE ACTUAL SQUARES
+	# BY FINDING THEIR WIDTH AND HEIGHT AND ONLY ACCEPTING ONES WITH SIMILAR W AND H
+	blurred = cv2.GaussianBlur(img, (5, 5), 0)
+	# thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, THRESH_VALUES.get(i, 9), 2)
+	thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
+	thresh[thresh > 0] = 255
+	# `thresh` has detected lines in (0,0,0) and everything else is (255,255,255) > invert for HoughLinesP
+	thresh = cv2.bitwise_not(thresh)
+	# lines = cv2.HoughLines(thresh, rho=1, theta=np.pi/180, threshold=156, lines=None, srn=0, stn=0)
+	lines = cv2.HoughLinesP(thresh, rho=1, theta=np.pi/180, threshold=255, minLineLength=20, maxLineGap=1)
+
+	# print(i, "|", len(lines))
+
+	out = thresh.copy()
+	out = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
+	
+	for line in lines:
+		# HoughLines
+		if len(line[0]) == 2:
+			rho, theta = line[0]
+			a = math.cos(theta)
+			b = math.sin(theta)
+			x0 = a * rho
+			y0 = b * rho
+			pt1 = (int(x0 + 30*(-b)), int(y0 + 30*(a)))
+			pt2 = (int(x0 - 30*(-b)), int(y0 - 30*(a)))
+
+			cv2.line(out, pt1, pt2, randomColor(), 1)
+		# HoughLinesP
+		elif len(line[0]) == 4:
+			for x1, y1, x2, y2 in line:
+				cv2.line(out, (x1, y1), (x2, y2), randomColor(), 2)
+
+	# cv2.imshow(f"Threshold'd{i}", thresh)
+	cv2.imshow(f"Found lines {i}", out)
 
 # Filter out purple 
-hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # blob_mask = cv2.inRange(hsv_image, (135, 0, 0), (160, 255, 255))
 # contours = cv2.findContours(
@@ -38,26 +117,19 @@ hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 # total_area = sum([ cv2.contourArea(c) for c in contours ])
 # print(f"Total area in pixels: {total_area}")
 
+# idx -> thresh value
+THRESH_VALUES = {
+	9:	31,	10:	31,	12:	31,	13:	31,	14:	31,	15:	31,	17:	31,	18:	31,	19:	31,
+	16:	17
+}
+
+# random.shuffle(images)
+
 for i, img in enumerate(images):
-	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-	# -> Grayscale
-	lines_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	# Find lines via threshold
-	thresh = cv2.adaptiveThreshold(lines_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 15)
-	lines_image[thresh == 255] = 255
-	lines_image = cv2.bitwise_not(lines_image)
-	cv2.imshow(f"Contrast{i}", cv2.resize(lines_image, None, fx=1/2, fy=1/2))
+	findGridLines(img)	
+	# continue
+	# break
 	
-	lines = cv2.HoughLinesP(lines_image, 1, np.pi/180, 1, np.array([]), 150, 2)
-	lines_image = cv2.cvtColor(lines_image, cv2.COLOR_GRAY2BGR)
-	for line in lines:
-		for x1, y1, x2, y2 in line:
-			cv2.line(lines_image, (x1, y1), (x2, y2), (255, 255, 0), 2)
-
-	cv2.imshow(f"Detected Lines{i}", lines_image)
 cv2.waitKey(0)
-
-# Find lines
 
 # cv2.imwrite(LOCAL / "out.png", lines_image)
