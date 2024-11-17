d124f79f6cfa22cca0320ec7ddbbe09b166d39bd
Brxnni <arne.norvin@gmail.com>
2024-11-17 20:38:06 +0100

    We can now differentiate between horizontal, vertical and irrelevant grid lines

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pathlib
 LOCAL = pathlib.Path(__file__).parent
 images = [ cv2.imread(LOCAL / "w12images" / path) for path in os.listdir(LOCAL / "w12images") ]
 
-def showImage(title, i):
+def showImage(title, i, scale=True):
 	max_h, h = len(i), 900
 	max_w, w = len(i[0]), 1920
 
@@ -17,7 +17,7 @@ def showImage(title, i):
 	mw = max_w / w
 
 	m = max(mh, mw)
-	if m != 1:
+	if m != 1 and scale:
 		i = cv2.resize(i, None, fx=1/m, fy=1/m)
 	
 	cv2.imshow(title, i)
@@ -34,12 +34,14 @@ def randomColor():
 		)
 	)
 
-# Theta [0, 1] -> Color
-def angleToColor(theta):
+# Theta [-π, π] -> Bright Color
+def angleToColor(θ):
+	# -> [0, 2π]
+	θ += np.pi
 	return tuple(
 		reversed(
 			[ round(i*255) for i in colorsys.hsv_to_rgb(
-				abs(theta)/(2*np.pi), 1, 1
+				θ/(2*np.pi), 1, 1
 			)]
 		)
 	)
@@ -80,10 +82,9 @@ def findSquareContours(i):
 def findGridLines(i):
 	img = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
 
-	# vv THIS CAN WORK!! FIND THRESH VALUES SUCH THAT ALL TINY 1mm² SQUARES ARE SEPERATE,
-	# THEN FIND ALL WHITE BLOBS (WHICH SHOULD INCLUDE SQUARES), FILTER FOR THE ACTUAL SQUARES
-	# BY FINDING THEIR WIDTH AND HEIGHT AND ONLY ACCEPTING ONES WITH SIMILAR W AND H
-	blurred = cv2.GaussianBlur(img, (5, 5), 0)
+	blurred = cv2.GaussianBlur(img, (11, 11), 0)
+	# blurred = cv2.dilate(blurred, np.ones((5, 5)))
+	# blurred = img.copy()
 	# thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, THRESH_VALUES.get(i, 9), 2)
 	thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
 	thresh[thresh > 0] = 255
@@ -93,26 +94,57 @@ def findGridLines(i):
 
 	out = thresh.copy()
 	out = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
-	
-	M = []
 
+	lines_objs = []
 	for line in lines:
-		if len(line[0]) == 4:
-			x1, y1, x2, y2 = line[0]
+		for (x1, y1, x2, y2) in line:
+			# In OpenCV, (0, 0) is in the top left, but we calculate the slope as if it was in the bottom left
+			# -> dx = -(x2-x1) instead of x2-x1
+			θ = abs(math.atan2(y2-y1, -(x2-x1)))
 			
-			if x2 == x1:	m = np.inf
-			else:			m = (y2-y1)/(x2-x1)
-			M.append(float(m))
-			
-			# color = (255, 0, 0) if abs(m) > 10 else (0, 0, 255)
-			color = angleToColor(math.atan(m))
-			cv2.line(out, (x1, y1), (x2, y2), color, 2)
+			lines_objs.append({
+				"p1": (x1, y1),
+				"p2": (x2, y2),
+				"θ": θ
+			})
 
-	M.sort()
-	with open(LOCAL / "out.txt", "w+") as file: file.write("\n".join([ str(n) for n in M ]))
+	# print([ line["θ"] for line in lineslines_objs ])
+
+	hist, bin_edges = np.histogram([ obj["θ"] for obj in lines_objs ], bins = 10)
+
+	# Find two most common angles (these correspond to the vertical and horizontal lines)
+	bin_idx1, bin_idx2 = np.argpartition(hist, -2)[-2:]
+	θ_range1 = (bin_edges[bin_idx1], bin_edges[bin_idx1+1])
+	θ_range2 = (bin_edges[bin_idx2], bin_edges[bin_idx2+1])
+
+	# Find lines that have those angles (i.e. find vertical and horizontal lines)
+	lines1 = [ line for line in lines_objs if θ_range1[0] <= line["θ"] <= θ_range1[1] ]
+	lines2 = [ line for line in lines_objs if θ_range2[0] <= line["θ"] <= θ_range2[1] ]
+
+	# Debug drawing
+	# Green/Pink = Horizontal/Vertical, Blue = Neither, irrelevant
+	for line in lines_objs:
+		# if line not in lines1 and lines2:
+		cv2.line(out, line["p1"], line["p2"], (255, 0, 0), 2)
+	for line in lines1:
+		cv2.line(out, line["p1"], line["p2"], (255, 0, 255), 2)
+	for line in lines2:
+		cv2.line(out, line["p1"], line["p2"], (0, 255, 0), 2)
+
+	# Find t-values of all lines so that we can find the average distance between them
+	# (i.e. the cell size of the graph paper)
+	t_values1 = [  ]
+
+	# !!; The only things left to do are: Finish the calculation of the t-values of all the lines (see purpose above),
+	# do the final calculation for the area of the blob (very simple)
+	# But most importantly, improve the line finding process, either by making the `thresh` image better or by choosing bettrr params for HoughLinesP
+
+	print(hist, bin_edges)
+	print(θ_range1, θ_range2)
 
 	# cv2.imshow(f"Threshold'd{i}", thresh)
-	showImage(f"Found lines {i[0][:3]}", out)
+	showImage(f"Found lines {i[0][:3]}", out, True)
+	cv2.imwrite(LOCAL / "gridlines.png", out)
 
 def isDropShaped(c):
 	area = cv2.contourArea(c)
@@ -170,59 +202,33 @@ def findDropWithContours(idx, i):
 		total_area = sum([ cv2.contourArea(c) for c in contours ])
 	print(f"Total area in pixels: {total_area}")
 
-def findDropWithBlobs(idx, i):
+def findDropWithComps(idx, i):
 	# i = cv2.resize(i, None, fx=4, fy=4)
 	# Filter out purple 
 	hsv_image = cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
 
 	# Dark Purple
 	mask_1 = cv2.inRange(hsv_image, (270/360*180, 0, 0), (340/360*180, 255, 90))
-	# Black
+	# Black (Dark Gr[ae]y)
 	mask_2 = cv2.inRange(hsv_image, (0, 0, 0), (255, 255, 20))
 	blob_mask = cv2.bitwise_or(mask_1, mask_2)
 
-	cv2.imwrite(LOCAL / "out0.png", blob_mask)
-	showImage(f"Block Mask {idx}", blob_mask)
+	showImage(f"Blob Mask {idx}", blob_mask)
+	cv2.imwrite(LOCAL / "blobmask.png", blob_mask)
 
 	_, _, stats, _ = cv2.connectedComponentsWithStats(blob_mask)
 	blob = sorted(list(stats)[1:], key=lambda row: -row[4])[0]
 	area = blob[4]
 	print(f"Total area in pixels: {area}")
 
-	return
-
-	# Visualize found blobs
-	out = i.copy()
-	# out = cv2.bitwise_and(i, i, mask=blob_mask)
-	for idx, c in enumerate(contours):
-		color = (255, 0, 0) if idx == 0 else (0, 0, 255)
-		cv2.drawContours(out, contours, idx, color, 5)
-	showImage(f"Found purple drop {idx}", out)
-	cv2.imwrite(LOCAL / "out1.png", out)
-
-	total_area = 0
-	if len(contours) > 0:
-		contours = contours[0]
-		# Calculate area of blobs
-		total_area = sum([ cv2.contourArea(c) for c in contours ])
-	print(f"Total area in pixels: {total_area}")
-
-# idx -> thresh value
-THRESH_VALUES = {
-	9:	31,	10:	31,	12:	31,	13:	31,	14:	31,	15:	31,	17:	31,	18:	31,	19:	31,
-	16:	17
-}
-
-I = list(enumerate(images))[5:]
+I = list(enumerate(images))[0:]
 # random.shuffle(I)
 
 for i, img in I:
-	# findGridLines(img)
-	findDropWithBlobs(i, img)
+	findGridLines(img)
+	# findDropWithComps(i, img)
 	print(i)
-	break
+	# break
 	# continue
 
 cv2.waitKey(0)
-
-# cv2.imwrite(LOCAL / "out.png", lines_image)
